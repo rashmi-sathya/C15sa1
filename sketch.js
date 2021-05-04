c60ec484e2648ec61a919a1f281cef99ed05edc6
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-04 11:12:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 
 
 var car={
+ //Mention the car properties in key value pair
  
 };
 function setup() {
   createCanvas(400, 400);
-  console.log(car.color);
-  console.log(car.capacity);
+ //fill the object need to display
+  console.log(car.);
+  
 }
 
 function draw() {
